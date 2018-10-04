88b8b9f751a484cb560b674ec83c2765a9aaaf21
Katzeilla <hello@katzeilla.org>
2018-10-04 00:40:50 -0400

    Shellharden & Change variable name

--- scripts/set_password.sh
+++ scripts/set_password.sh
@@ -2,38 +2,38 @@
 
 # Generate a random password for admin if $MONGODB_ADMIN_PASS not set
 
-if [[ $ADMIN_PASSWORD == "" ]]; then 
+if [[ "$ADMIN_PASSWORD" == "" ]]; then 
 	ADMIN_PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 fi
 
 # Set admin username to admin if $ADMIN_USERNAME not set
 
-if [[ $ADMIN_USERNAME == "" ]]; then 
+if [[ "$ADMIN_USERNAME" == "" ]]; then 
 	ADMIN_USERNAME="admin"
 fi
 
 # Application Database User
 
 
-if [[ $APPLICATION_USERNAME == "" ]]; then 
+if [[ "$APPLICATION_USERNAME" == "" ]]; then 
 	APPLICATION_USERNAME="app"
 fi
 
-if [[ $APPLICATION_DATABASE == "" ]]; then 
+if [[ "$APPLICATION_DATABASE" == "" ]]; then 
 	APPLICATION_DATABASE="app"
 fi
 
-if [[ $APPLICATION_PASSWORD == "" ]]; then 
-	APPLICATION_PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+if [[ "$APPLICATION_PASSWORD" == "" ]]; then 
+	APPLICATION_PASSWORD="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
 fi
 
 # Wait for mongod to boot
-RET=1
+Mongod_status=1
 while [[ RET -ne 0 ]]; do
 	      echo "==>  Wait for mongod to boot..."
               sleep 5
 	      mongo admin --eval "help" >/dev/null 2>&1
-              RET=$?
+              Mongod_status=$?
 done
 
 # Create the admin user
@@ -43,10 +43,10 @@ mongo admin --eval "db.createUser({user: '$ADMIN_USERNAME', pwd: '$ADMIN_PASSWOR
 sleep 3
 
 # Create the application user
-echo "=> Creating an ${APPLICATION_DATABASE} user with a password in MongoDB"
-mongo admin -u $ADMIN_USERNAME -p $ADMIN_PASSWORD << EOF
-use $APPLICATION_DATABASE
-db.createUser({user: '$APPLICATION_USERNAME', pwd: '$APPLICATION_PASSWORD', roles:[{role:'readWrite', db:'$APPLICATION_DATABASE'}]})
+echo "=> Creating an $APPLICATION_DATABASE user with a password in MongoDB"
+mongo admin -u "$ADMIN_USERNAME" -p "$ADMIN_PASSWORD" << EOF
+use "$APPLICATION_DATABASE"
+db.createUser({user: '"$APPLICATION_USERNAME"', pwd: '"$APPLICATION_PASSWORD"', roles:[{role:'readWrite', db:'"$APPLICATION_DATABASE"'}]})
 EOF
 
 sleep 1
